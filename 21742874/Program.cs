cb0299c7da545ed109f7ff57141574637227ceaf
zeynepsulemir <61383292+zeynepsulemir@users.noreply.github.com>
2020-03-01 22:52:24 +0300

    değişiklik

--- 21742874/Program.cs
+++ 21742874/Program.cs
@@ -6,7 +6,7 @@ namespace _21742874
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Zeynep Şule Mirvelioğlu -- Ödev1");
         }
     }
 }
